466cf9038736077a0372f9b88e8fb607d5071b47
Sidhant Panda <sidhantpanda@gmail.com>
2014-04-12 18:49:16 +0530

    ford fulkerson implementation

---
+++ ford-fulkerson/implementation.py
@@ -0,0 +1,14 @@
+from ford_fulkerson import *
+
+g = FlowNetwork()
+for v in "sopqrt":
+    g.add_vertex(v)
+g.add_edge('s','o',3)
+g.add_edge('s','p',3)
+g.add_edge('o','p',2)
+g.add_edge('o','q',3)
+g.add_edge('p','r',2)
+g.add_edge('r','t',3)
+g.add_edge('q','r',4)
+g.add_edge('q','t',2)
+print g.max_flow('s','t')
